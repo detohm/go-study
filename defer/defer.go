b402db58ea2898129d1312cc41c7a10ec526af98
Ohm Attaphong <ohm.attaphong@gmail.com>
2017-08-07 23:58:40 +0700

    Add more experiment for defer command

--- defer/defer.go
+++ defer/defer.go
@@ -6,10 +6,12 @@ func main() {
 
 	fmt.Println("1")
 
-	defer fmt.Println("5")
+	defer fmt.Println("7")
+	defer fmt.Println("6")
 
 	fmt.Println("2")
 	fmt.Println("3")
 	fmt.Println("4")
 
+	defer fmt.Println("5")
 }
