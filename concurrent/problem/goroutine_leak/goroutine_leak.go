fd057f722f094d632a3415a72802d528a3656b2e
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2022-03-18 18:21:32 +0700

    Improve goroutine leak experiment by adding observer helper

--- concurrent/problem/goroutine_leak/goroutine_leak.go
+++ concurrent/problem/goroutine_leak/goroutine_leak.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
 func main() {
 
+	// --- non-related leak code
+	quitObserver := make(chan bool)
+	go observer(quitObserver)
+	defer func() {
+		quitObserver <- true
+	}()
+	// ---
+
 	// simulate long running process
 	for {
 		// passing nil channel
+		time.Sleep(time.Nanosecond)
 		go task(nil)
 	}
 }
@@ -20,7 +31,37 @@ func task(in chan int) {
 	}
 }
 
+// helper function for observing num of goroutines
+func observer(quit chan bool) {
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			time.Sleep(time.Second)
+			fmt.Printf("num of goroutines: %d\n", runtime.NumGoroutine())
+		}
+	}
+}
+
 /* Result
 Memory consumption will be increasing until out of memory occurs
-It's go routines leak as task in go routine cannot be completed
+This is goroutines leak issue as a task in goroutine cannot be completed
+due to blocked from nil channel.
+
+The number of runtime goroutines will keep increasing
+which affects memory consumption.
+
+Output:
+num of goroutines: 370824
+num of goroutines: 761902
+num of goroutines: 1266289
+num of goroutines: 1573220
+num of goroutines: 2067518
+num of goroutines: 2347368
+num of goroutines: 2694127
+num of goroutines: 3122206
+num of goroutines: 3561923
+num of goroutines: 3943206
+num of goroutines: 4289707
 */
