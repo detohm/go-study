2626210461256213f269840cbbde1f85d9d7dfb9
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2022-06-12 11:11:46 +0700

    Experiment with custom channel writer

--- io/writer/writer.go
+++ io/writer/writer.go
@@ -14,6 +14,7 @@ type Writer interface {
 
 func main() {
 	// --- experiment with standard lib writer (bytes) ---
+	fmt.Println("--- standard lib writer")
 	data := []string{
 		"Thailand",
 		"USA",
@@ -36,4 +37,46 @@ func main() {
 	// output
 	// ThailandUSAChinaIndia
 
+	// --- custom writer ---
+	fmt.Println("--- custom channel writer")
+	cWriter := NewChanWriter()
+	go func() {
+		defer cWriter.Close()
+		cWriter.Write([]byte("stream data"))
+		cWriter.Write([]byte("this is a book"))
+	}()
+
+	for c := range cWriter.Chan() {
+		fmt.Printf("%c", c)
+	}
+	fmt.Println()
+	// output
+	// stream datathis is a book
+}
+
+// -- custom writer ---
+type chanWriter struct {
+	ch chan byte
+}
+
+func NewChanWriter() *chanWriter {
+	return &chanWriter{make(chan byte, 1)}
+}
+
+func (w *chanWriter) Chan() <-chan byte {
+	return w.ch
+}
+
+func (w *chanWriter) Write(p []byte) (int, error) {
+	n := 0
+	for _, b := range p {
+		w.ch <- b
+		n++
+	}
+	return n, nil
+}
+
+func (w *chanWriter) Close() error {
+	close(w.ch)
+	return nil
 }
