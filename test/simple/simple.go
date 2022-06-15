d46397425bf6ff5bcce856211e28292c05f032e7
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2022-06-15 22:59:53 +0700

    Playaround with unit tests - Test, Benchmark, Example

---
+++ test/simple/simple.go
@@ -0,0 +1,9 @@
+package main
+
+func Mul(a int, b int) int {
+	return a * b
+}
+
+func Cus(a int, b int) int {
+	return a * a * b * b
+}
---
+++ test/simple/simple_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMul(t *testing.T) {
+	got := Mul(2, 3)
+	expected := 6
+
+	if got != expected {
+		t.Errorf("got %v, wanted %v", got, expected)
+	}
+}
+
+type MulCase struct {
+	arg1     int
+	arg2     int
+	expected int
+}
+
+func TestMulTable(t *testing.T) {
+	testcases := []MulCase{
+		{2, 3, 6},
+		{4, 8, 32},
+		{10, 20, 200},
+	}
+	for _, tc := range testcases {
+		if got := Mul(tc.arg1, tc.arg2); got != tc.expected {
+			t.Errorf("got %v, expected %v", got, tc.expected)
+		}
+	}
+}
+
+func BenchmarkMul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Mul(2, 3)
+	}
+}
+
+func ExampleMul() {
+	fmt.Println(Mul(2, 3))
+	// Output: 6
+}
